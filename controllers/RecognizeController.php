3a0125671d20bcdd1964610fde0218e7a392116b
zikwall <andrey.kapitonov.96@gmail.com>
2019-11-09 21:42:49 +0300

    Review Code

--- controllers/RecognizeController.php
+++ controllers/RecognizeController.php
@@ -19,6 +19,13 @@ class RecognizeController extends Controller
         return parent::beforeAction($action);
     }
 
+    public function actionConfirm($face)
+    {
+        return $this->asJson([
+            'res' => self::confirm($face)
+        ]);
+    }
+
     public function actionFace()
     {
         if (Yii::$app->request->getIsOptions()) {
@@ -53,31 +60,20 @@ class RecognizeController extends Controller
         ]);
     }
 
-    public static function send($filename, $targetFile, $options = [])
+    public static function confirm($id)
     {
-        if(!is_file($targetFile)) {
-            throw new \Exception('Target file is not a file or not found!');
-        }
-
-        $uploadRequest = [
-            'uploaded_photo' => new \cURLFile($targetFile, $extension, $filename),
-        ];
-
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, Module::module()->handleUrl);
+        curl_setopt($curl, CURLOPT_URL, sprintf('%s/upload3?id=%d', Module::module()->handleUrl, $id));
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,550000000);
         curl_setopt($curl, CURLOPT_TIMEOUT, 5500000000); //timeout in seconds
-        curl_setopt($curl, CURLOPT_HTTPHEADER, self::getHeaders());
-        curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $uploadRequest);
         curl_setopt($curl, CURLOPT_HTTPHEADER, [
             'Accept: application/json',
             'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
             'Expect:',
             'Content-Type: multipart/form-data',
-            'session-key: ' . Module::module()->securityToken,
-            'user-id: ' . Module::module()->userId
+            sprintf('session-key: %s', Module::module()->securityToken),
+            sprintf('user-id: %d', Module::module()->userId)
         ]);
 
         $response = curl_exec($curl);
@@ -95,20 +91,44 @@ class RecognizeController extends Controller
         return $response;
     }
 
-    public static function getHeaders()
+    public static function send($filename, $targetFile, $options = [])
     {
-        $headers = array();
-        $headers[] = 'Cache-Control: no-cache';
-        $headers[] = 'Connection: Keep-Alive';
-        $headers[] = 'Keep-Alive: 600';
-        $headers[] = 'Accept-Language: en-us';
-        $headers[] = 'X-CSRFToken: SeN9bHryRK8FWLTLJIs5c6u9AZ47a8pR';
+        if(!is_file($targetFile)) {
+            throw new \Exception('Target file is not a file or not found!');
+        }
 
-        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-        $headers[] = 'Origin: https://domain.com';
-        $headers[] = 'Referer: https://domain.com';
+        $uploadRequest = [
+            'uploaded_photo' => new \cURLFile($targetFile, $options['ext'], $filename),
+        ];
 
-        return $headers;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, sprintf('%s/%s', Module::module()->handleUrl, 'upload2'));
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,550000000);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 5500000000); //timeout in seconds
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $uploadRequest);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+            'Expect:',
+            'Content-Type: multipart/form-data',
+            sprintf('session-key: %s', Module::module()->securityToken),
+            sprintf('user-id: %d', Module::module()->userId)
+        ]);
 
+        $response = curl_exec($curl);
+
+        if($errno = curl_errno($curl)) {
+            $error_message = curl_strerror($errno);
+            echo "cURL error ({$errno}):\n {$error_message}";
+            exit;
+        }
+
+        $response = json_decode($response, true);
+
+        curl_close($curl);
+
+        return $response;
     }
 }
